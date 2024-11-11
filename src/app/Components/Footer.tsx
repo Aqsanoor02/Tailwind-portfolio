b53e1dda851444eba78c1bdbb6dc69c3f2dc2fa7
qsanoor02 <aqsan753@gmail.com>
2024-11-11 19:57:33 +0500

    Fixed stuff

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -12,7 +12,7 @@ function Footer() {
           <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
           <Image alt="image" height={40} width={40} src="/Assets/Alogo.png"></Image>
 
-            <span className="ml-3 text-xl">A Noor's Corner</span>
+            <span className="ml-3 text-xl">A Noor&apos;s Corner</span>
           </a>
           <p className="text-sm text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
             © 2024 ANC
--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -15,7 +15,7 @@ function Navbar() {
               src="/Assets/Alogo.png"
             ></Image>
             <span className="text-teal-100 ml-3 text-xl  ">
-              A Noor's Corner
+              A Noor&apos;s Corner
             </span>
           </a>
           <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
